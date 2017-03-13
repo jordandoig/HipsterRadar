37d2ab438105dc180af7423c0e9a1c4492954ace
Jordan Doig <jordandoig@live.com>
2017-03-13 13:30:36 -0600

    Map click moves marker

--- app.js
+++ app.js
@@ -1,23 +1,34 @@
 //Google maps API key
 //AIzaSyCbLFtIwl7aH8u4vkTY0I0X5oZmLuezv3A
 
-$(document).ready(function(){
-  getMap();
-});
+// $(document).ready(function(){
+//   getMap();
+// });
 
-var src = "https://maps.googleapis.com/maps/api/js?key=AIzaSyCbLFtIwl7aH8u4vkTY0I0X5oZmLuezv3A";
+//var src = "https://maps.googleapis.com/maps/api/js?key=AIzaSyCbLFtIwl7aH8u4vkTY0I0X5oZmLuezv3A";
 
-var map;
+var map = document.querySelector(".googleMap");
+
+var loc = {
+  lat: 39.7392,
+  lng: -104.9903
+};
 
 function getMap () {
-  var loc = {lat: 39.7392, lng: -104.9903};
-  map = new google.maps.Map(document.getElementById("googleMap"), {
+  var map = new google.maps.Map(document.querySelector(".googleMap"), {
     zoom: 13,
     center: loc
   });
   var marker = new google.maps.Marker({
     position: loc,
     map: map
+  });
+  map.addListener("click", function(event) {
+    loc = {
+      lat: event.latLng.lat(),
+      lng: event.latLng.lng()
+    };
+    getMap();
   });
 }
 
